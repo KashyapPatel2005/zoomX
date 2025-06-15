15a31643901de766ac9bb98f319e1b3d8bd4a2d8
Kashyap Patel <kp2037723@gmail.com>
2025-06-15 10:46:46 +0530

    change client url

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use(cors({
 app.use(express.json({limit:"40kb"}))
 app.use(express.urlencoded({limit:"40kb", extended:true}))
 
-app.use("/api/v1/users",userRoutes);
+app.use("/",userRoutes);
 
 // app.get("/",(req,res)=>{
 //     res.send('Hii');
